180f8723708e2b3a19fba687c35324e7337cce8d
Stanley Lee <stanleylee91776@gmail.com>
2018-10-14 20:56:53 -0700

    app

---
+++ bot.js
@@ -0,0 +1,31 @@
+var Botkit = require('./lib/Botkit.js');
+
+var controller = Botkit.sparkbot({
+    debug: true,
+    log: true,
+    public_address: process.env.public_address,
+    ciscospark_access_token: process.env.access_token,
+    secret: process.env.secret
+});
+
+
+var bot = controller.spawn({
+});
+
+controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function() {
+        console.log("SPARK: Webhooks set up!");
+    });
+});
+
+controller.hears('hello', 'direct_message,direct_mention', function(bot, message) {
+    bot.reply(message, 'Hi');
+});
+
+controller.on('direct_mention', function(bot, message) {
+    bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
+});
+
+controller.on('direct_message', function(bot, message) {
+    bot.reply(message, 'I got your private message. You said, "' + message.text + '"');
+});
